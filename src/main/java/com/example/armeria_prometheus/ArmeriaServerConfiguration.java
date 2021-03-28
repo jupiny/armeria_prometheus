6a4c15094e6c56a55670b68173dea0e1f37d3c2c
seunghwan-joo <seunghwan.joo@linecorp.com>
2021-03-28 17:14:02 +0900

    Add PrometheusExpositionService

--- src/main/java/com/example/armeria_prometheus/ArmeriaServerConfiguration.java
+++ src/main/java/com/example/armeria_prometheus/ArmeriaServerConfiguration.java
@@ -3,13 +3,22 @@ package com.example.armeria_prometheus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.linecorp.armeria.server.metric.PrometheusExpositionService;
 import com.linecorp.armeria.spring.ArmeriaServerConfigurator;
+
+import io.micrometer.prometheus.PrometheusMeterRegistry;
 
 @Configuration
 public class ArmeriaServerConfiguration {
 
     @Bean
+    public ArmeriaServerConfigurator prometheusConfigurator(PrometheusMeterRegistry registry) {
+        return server -> server.service("/metrics",
+                                        new PrometheusExpositionService(registry.getPrometheusRegistry()));
+    }
+
+    @Bean
     public ArmeriaServerConfigurator serverConfigurator(MyAnnotatedService myService) {
-        return server -> server.annotatedService("/", myService);
+        return server -> server.annotatedService("/jupiny", myService);
     }
 }
