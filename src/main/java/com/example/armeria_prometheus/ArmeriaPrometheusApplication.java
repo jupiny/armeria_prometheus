55c7663577653594785a3e5493de191809c91cc7
seunghwan-joo <seunghwan.joo@linecorp.com>
2021-04-22 01:23:24 +0900

    Remove spring autoconfigure dependencies

--- src/main/java/com/example/armeria_prometheus/ArmeriaPrometheusApplication.java
+++ src/main/java/com/example/armeria_prometheus/ArmeriaPrometheusApplication.java
@@ -1,12 +1,32 @@
 package com.example.armeria_prometheus;
 
-import org.springframework.boot.SpringApplication;
+import java.util.concurrent.CompletableFuture;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.linecorp.armeria.common.metric.MeterIdPrefixFunction;
+import com.linecorp.armeria.server.Server;
+import com.linecorp.armeria.server.metric.MetricCollectingService;
+import com.linecorp.armeria.server.metric.PrometheusExpositionService;
+
+import io.micrometer.prometheus.PrometheusConfig;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
 
 @SpringBootApplication
 public class ArmeriaPrometheusApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ArmeriaPrometheusApplication.class, args);
+        PrometheusMeterRegistry meterRegistry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+        Server server = Server
+                .builder()
+                .http(8083)
+                .meterRegistry(meterRegistry)
+                .annotatedService(new MyAnnotatedService(meterRegistry))
+                .decorator(MetricCollectingService.newDecorator(MeterIdPrefixFunction.ofDefault("my.server")))
+                .service("/metrics", new PrometheusExpositionService(meterRegistry.getPrometheusRegistry()))
+                .build();
+
+        CompletableFuture<Void> future = server.start();
+        future.join();
     }
 }
--- src/main/java/com/example/armeria_prometheus/MyAnnotatedService.java
+++ src/main/java/com/example/armeria_prometheus/MyAnnotatedService.java
@@ -2,15 +2,12 @@ package com.example.armeria_prometheus;
 
 import java.util.Random;
 
-import javax.inject.Named;
-
 import com.linecorp.armeria.common.HttpResponse;
 import com.linecorp.armeria.server.annotation.Get;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
 
-@Named
 public class MyAnnotatedService {
 
     private static Random RAND = new Random();
